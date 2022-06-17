dba90843bfe821d13712d3aaa2944eaa892fa7f9
14212114 <mukuha.nelly@gmail.com>
2022-06-17 19:42:25 +0300

    commit tree 988d9d3c1ad31d97452a913b123dd97ac505e607

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Follow along with the examples here
+function doNothing() {}
+
+function sayHello() {
+    console.log("Hello!");
+  }
+  sayHello();
+
+  function sayHelloToGuadalupe() {
+    console.log("Hello, Guadalupe!");
+  }
+  sayHelloToGuadalupe();
+
+  function sayHelloToLiz() {
+    console.log("Hello, Liz!");
+  }
+  sayHelloToLiz();
+
+  function sayHelloToSamip() {
+    console.log("Hello, Samip!");
+  }
+  sayHelloToSamip();
+
+sayHelloToGuadalupe();
+sayHelloToLiz();
+sayHelloToSamip();
+
+function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+  }
+say("Goodbye", "Julio");
+
+function add(x, y) {
+    return x + y;
+  }
+  console.log(add(80, 9000));
